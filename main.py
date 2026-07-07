fd426e0b4a12c2465ec69e855cbd95f4eb92eed9
kidus-yo <kidusyonas96@gmail.com>
2026-07-07 23:55:51 +0300

    The first Code

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
 #restaurant-sytem
+def main():
+   main_menu()
+
+
+
+def main_menu():
+    print("Main Menu")
+    print("1. Show food Menu")
+    print("2. Take Order")
+    print("3. View Cart")
+    print("4. remove Item")
+    print("5. Checkout")
+    print("6. Clear Cart")
+    print("7. Exit❎")
+
+    choice = int(input("Please Enter your choice?: "))
+    print(choice)
+
+main() 
+
+
+def take_order():
+    print("Kidus Restaurnt Menu:")
+    print("1. Hamburgers..........$50")
+    print("2. ")
+
